daacc9818aaab361cc074f2c805b6712a7b70536
BencWislla <beniciowislla@gmail.com>
2026-01-31 14:51:18 +0100

    create setup

---
+++ src/components/ui/Button/styles.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components'
+
+//Cor botao vermelho #dc2626
+//Cor botao cinza #9ca3af
+//Cor botao azul #2563eb
+
+export const ButtonContainer = styled.button`
+background-Color: ${(props) => props.color};
+color: ${(props => props.colorText)};
+border-radius: .5rem;
+padding: .75rem 1.5rem;
+border: none;
+font-weight: 500;
+transition: all 200ms ease-in-out;
+cursor: pointer;
+
+&:disabled{
+  opacity: 0.5;
+  cursor: not-allowed;
+}
+&:hover:not(:disabled) {
+      opacity:0.9;
+    }
+`;
---
+++ src/components/ui/Input/styles.js
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+
+
+export const InputContainerWrapper = styled.div`
+width:100%;
+display: flex;
+flex-direction: column;
+gap:1rem ;
+`
+
+
+export const LabelContainer = styled.label`
+ color: #364153;
+ font-size: .875rem;
+`;
+export const SpanText = styled.span`
+  color: #FB2C36;
+`;
+export const InputContainer = styled.div`
+  padding: .75rem 1rem;
+  border: .0625rem solid #d1d5db;
+  border-radius:.5rem;
+  outline: none;
+
+`
+export const InputContent = styled.input`
+  border: none;
+  outline: none;
+  font-size:1rem;
+  width: 100%;
+   &::placeholder{
+     color: #9ca3af ;
+   }
+
+`
